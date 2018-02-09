56d510a28395ca2caae1273cd333f463bc7b4b86
Evan Candler <emcandler21@gmail.com>
2018-02-09 20:54:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  new_array = []
+    attendees.each |name| new_array << badge_maker(name)
+  return new_array
+end
